3387c9a53828164099f9c3e06561173ed44d327d
meulk <51929618+meulk@users.noreply.github.com>
2022-02-27 10:54:04 +0000

    Update setup-pidoh.sh

--- setup-pidoh.sh
+++ setup-pidoh.sh
@@ -8,7 +8,6 @@
 set -e
 # Any subsequent(*) commands which fail will cause the shell script to exit immediately
 
-
 # Download Cloudflared - arm64 architecture (64-bit Raspberry Pi)
 wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64
 sudo cp ./cloudflared-linux-arm64 /usr/local/bin/cloudflared
@@ -27,15 +26,10 @@ sudo systemctl start cloudflared
 #sudo systemctl status cloudflared
 
 # Automating Cloudflared Updates
-#echo "sudo cloudflared update" >> /etc/cron.weekly/cloudflared-updater
-#echo "sudo systemctl restart cloudflared" >> /etc/cron.weekly/cloudflared-updater
-
 {
 echo "sudo cloudflared update"
 echo "sudo systemctl restart cloudflared"
 }>> /etc/cron.weekly/cloudflared-updater
-
-
 sudo chmod +x /etc/cron.weekly/cloudflared-updater
 sudo chown root:root /etc/cron.weekly/cloudflared-updater
 
@@ -46,10 +40,9 @@ sudo chown root:root /etc/cron.weekly/cloudflared-updater
 dohDNS="PIHOLE_DNS_1=127.0.0.1#5053"
 target="/etc/pihole/setupVars.conf"
 
-# replace DNS with new DOH DNS
+# replace PIHOLE_DNS_1 with new DOH DNS
 sed -i "s/PIHOLE_DNS_1=1.1.1.1/$dohDNS/" "${target}"
-
-#remove DNS_2 line
+#remove PIHOLE_DNS_2 line
 sed -i '/^PIHOLE_DNS_2/d' "${target}"
 
 
