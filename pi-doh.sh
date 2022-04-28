a3a0ecb8076ae670c240008d40a0d4c9b4e9b1d1
meulk <51929618+meulk@users.noreply.github.com>
2022-04-28 09:48:07 +0100

    Update pi-doh.sh

--- pi-doh.sh
+++ pi-doh.sh
@@ -141,7 +141,8 @@ cleanup() {
 }
 
 Pi-alias() {
-	echo "alias piup='sudo apt-get update && sudo apt-get full-upgrade && sudo apt-get autoremove && sudo apt-get clean'" >> ~/.bash_aliases && source ~/.bash_aliases
+	echo "alias piup='sudo apt-get update && sudo apt-get full-upgrade && sudo apt-get autoremove && sudo apt-get clean'" >> ~/.bash_aliases
+	source ~/.bash_aliases
 	printf "${TICK} piup alias added.\n"
 }
 
